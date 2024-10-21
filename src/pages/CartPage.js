2abbc0787731354c232e9b1eeff0a8c76e5ecc5f
unknown <you@example.com>
2024-10-21 12:53:02 +0200

    stylepanier

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,32 +1,39 @@
-// src/components/CartPage.js
-
-import React from 'react';
-import { useCart } from '../components/CartContext';
+import React from "react";
+import { useCart } from "../components/CartContext";
+import "./css/CartPage.css"; // Assurez-vous d'importer le fichier CSS
 
 const CartPage = () => {
-    const { cart, removeFromCart, clearCart } = useCart(); // Récupérer le panier et les fonctions du contexte
+  const { cart, removeFromCart, clearCart } = useCart();
 
-    if (!cart || cart.length === 0) {
-        return <h2>Votre panier est vide.</h2>;
-    }
+  if (!cart || cart.length === 0) {
+    return <h2 className="cart-title">Votre panier est vide.</h2>;
+  }
 
-    const totalAmount = cart.reduce((acc, shoe) => acc + shoe.price, 0);
+  const totalAmount = cart.reduce((acc, shoe) => acc + shoe.price, 0);
 
-    return (
-        <div>
-            <h1>Votre Panier</h1>
-            <ul>
-                {cart.map((shoe) => (
-                    <li key={shoe.id}>
-                        {shoe.name} - {shoe.price} €
-                        <button onClick={() => removeFromCart(shoe.id)}>Supprimer</button>
-                    </li>
-                ))}
-            </ul>
-            <h2>Total: {totalAmount} €</h2>
-            <button onClick={clearCart}>Vider le Panier</button>
-        </div>
-    );
+  return (
+    <div className="cart-container">
+      <h1 className="cart-title">Votre Panier</h1>
+      <ul>
+        {cart.map((shoe) => (
+          <li className="cart-item" key={shoe.id}>
+            <span className="cart-item-name">{shoe.name}</span>
+            <span className="cart-item-price">{shoe.price} €</span>
+            <button
+              className="cart-button"
+              onClick={() => removeFromCart(shoe.id)}
+            >
+              Supprimer
+            </button>
+          </li>
+        ))}
+      </ul>
+      <h2 className="total-amount">Total: {totalAmount} €</h2>
+      <button className="cart-button" onClick={clearCart}>
+        Vider le Panier
+      </button>
+    </div>
+  );
 };
 
 export default CartPage;
