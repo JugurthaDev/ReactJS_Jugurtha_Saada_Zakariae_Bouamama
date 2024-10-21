d7c5e9de04ee46e481da0fc0f14306c1f058dcb6
Jugurtha <saadajugurtha59@gmail.com>
2024-10-21 12:55:25 +0200

    add style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,20 @@
-// src/components/Header.js
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './css/Header.css';
 import logo from './img/logo.png';
+import { FaShoppingCart } from 'react-icons/fa';
 
 function Header() {
     return (
         <header className="header">
             <img src={logo} alt="Logo Wenov" className="header-logo" />
-            <h1 className="header-title">Wenov Store</h1>
             <div className="header-buttons">
-                <Link to="/" className="header-button">
+                <Link to="/" className="header-button catalog-button">
                     Catalogue
                 </Link>
-                <Link to="/cart" className="header-button">
-                    Panier
+                <Link to="/cart" className="header-button cart-button">
+                    <FaShoppingCart className="cart-icon" />
+                    <span className="cart-count">0</span>
                 </Link>
             </div>
         </header>
